dda761d07294e83bd8994b74b713333861125b40
Dmitrii Bacherikov <dialex2006@yandex.ru>
2022-12-13 22:09:18 +0200

    Deleted test buttons

--- src/pages/AddAsset.tsx
+++ src/pages/AddAsset.tsx
@@ -75,20 +75,7 @@ const AddAsset = () => {
             ref={serialRef}
           ></textarea>
         </div>
-        <button
-          onClick={() => {
-            dispatch(inventoryActions.requestDeleteSBAssetById(5));
-          }}
-        >
-          TEST DELETION BY ID
-        </button>
-        <button
-          onClick={() => {
-            dispatch(inventoryActions.requestDeleteSBAssetByNumber("33333333"));
-          }}
-        >
-          TEST DELETION BY SERIAL NUMBER
-        </button>
+
         <button className="button">Submit information</button>
       </form>
     </div>
